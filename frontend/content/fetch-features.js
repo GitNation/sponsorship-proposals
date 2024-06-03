f606e3cf2ce9bb1834e37891216d4a17fe9db794
lero62 <weblero2020@gmail.com>
2024-06-03 15:06:09 +0300

    update schemas

--- frontend/content/fetch-features.js
+++ frontend/content/fetch-features.js
@@ -1,45 +1,46 @@
-const { getSvg } = require('./svg');
+const { getSvg } = require("./svg");
 
 const queryPages = /* GraphQL */ `
-  query($brandId: ID!) {
-    data: Brand(id: $brandId) {
-      features {
-        title
-        description
-        icon {
-          asset {
-            url
-          }
-        }
-      }
-    }
-  }
+	query ($brandId: ID!) {
+		data: Brand(id: $brandId) {
+			features {
+				title
+				subtitle
+				description
+				icon {
+					asset {
+						url
+					}
+				}
+			}
+		}
+	}
 `;
 
-const fetchData = async(client, vars) => {
-  const data = await client
-    .request(queryPages, vars)
-    .then(res => res.data.features);
+const fetchData = async (client, vars) => {
+	const data = await client
+		.request(queryPages, vars)
+		.then((res) => res.data.features);
 
-  if (!data) {
-    return {
-      features: null
-    };
-  }
+	if (!data) {
+		return {
+			features: null,
+		};
+	}
 
-  const features = data.map(async(feature, ind) => ({
-    ...feature,
-    svg: await getSvg(feature.icon),
-    animation: 'fade-up',
-    animDelay: 200 + ind * 100,
-    iconClass: `feature_${ind + 1}`,
-  }));
+	const features = data.map(async (feature, ind) => ({
+		...feature,
+		svg: await getSvg(feature.icon),
+		animation: "fade-up",
+		animDelay: 200 + ind * 100,
+		iconClass: `feature_${ind + 1}`,
+	}));
 
-  return {
-    features,
-  };
+	return {
+		features,
+	};
 };
 
 module.exports = {
-  fetchData,
+	fetchData,
 };
--- frontend/content/index.js
+++ frontend/content/index.js
@@ -1,89 +1,91 @@
-const { GraphQLClient } = require('graphql-request');
+const { GraphQLClient } = require("graphql-request");
 
-const { credentials, brandIDs, isDebug } = require('./config');
+const { credentials, brandIDs, isDebug } = require("./config");
 
-const brandContent = require('./fetch-brand');
-const infoContent = require('./fetch-info');
-const featuresContent = require('./fetch-features');
-const keyFeaturesContent = require('./fetch-key-features');
-const packagesContent = require('./fetch-packages');
-const partnersContent = require('./fetch-partners');
-const sponsorsContent = require('./fetch-sponsors');
-const statsContent = require('./fetch-stats');
-const tilesContent = require('./fetch-tiles');
+const brandContent = require("./fetch-brand");
+const infoContent = require("./fetch-info");
+const featuresContent = require("./fetch-features");
+const keyFeaturesContent = require("./fetch-key-features");
+const packagesContent = require("./fetch-packages");
+const partnersContent = require("./fetch-partners");
+const sponsorsContent = require("./fetch-sponsors");
+const statsContent = require("./fetch-stats");
+const tilesContent = require("./fetch-tiles");
+const conferencesContent = require("./fetch-conferences");
 
 const createClient = ({ endpoint, token }) => {
-  return new GraphQLClient(endpoint, {
-    headers: {
-      authorization: `Bearer ${token}`,
-    },
-  });
+	return new GraphQLClient(endpoint, {
+		headers: {
+			authorization: `Bearer ${token}`,
+		},
+	});
 };
 
 const client = createClient(credentials);
 
-const getContent = async() => {
-  const brandName = process.env.BRAND_NAME;
-  const brandId = brandIDs[brandName];
+const getContent = async () => {
+	const brandName = process.env.BRAND_NAME;
+	const brandId = brandIDs[brandName];
 
-  const fetchAll = [
-    brandContent,
-    infoContent,
-    featuresContent,
-    keyFeaturesContent,
-    packagesContent,
-    partnersContent,
-    sponsorsContent,
-    statsContent,
-    tilesContent,
-  ].map(async content => {
-    try {
-      return await content.fetchData(client, {
-        brandId,
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  });
+	const fetchAll = [
+		brandContent,
+		infoContent,
+		featuresContent,
+		keyFeaturesContent,
+		packagesContent,
+		partnersContent,
+		sponsorsContent,
+		statsContent,
+		tilesContent,
+		conferencesContent,
+	].map(async (content) => {
+		try {
+			return await content.fetchData(client, {
+				brandId,
+			});
+		} catch (err) {
+			console.error(err);
+		}
+	});
 
-  const contentArray = await Promise.all(fetchAll);
+	const contentArray = await Promise.all(fetchAll);
 
-  const contentArrayToResolve = contentArray.map(async obj => {
-    const entries = Object.entries(obj);
-    const toResolve = entries.map(async([key, val]) => [
-      key,
-      Array.isArray(val) ? await Promise.all(val) : await val,
-    ]);
-    const resolved = await Promise.all(toResolve);
-    const ObjectFromEntries = resolved.reduce(
-      (obj, [key, val]) => ({ ...obj, [key]: val }),
-      {}
-    );
-    return ObjectFromEntries;
-  });
+	const contentArrayToResolve = contentArray.map(async (obj) => {
+		const entries = Object.entries(obj);
+		const toResolve = entries.map(async ([key, val]) => [
+			key,
+			Array.isArray(val) ? await Promise.all(val) : await val,
+		]);
+		const resolved = await Promise.all(toResolve);
+		const ObjectFromEntries = resolved.reduce(
+			(obj, [key, val]) => ({ ...obj, [key]: val }),
+			{}
+		);
+		return ObjectFromEntries;
+	});
 
-  const contentArrayResolved = await Promise.all(contentArrayToResolve);
+	const contentArrayResolved = await Promise.all(contentArrayToResolve);
 
-  const contentMap = contentArrayResolved.reduce(
-    (content, piece) => ({ ...content, ...piece }),
-    {}
-  );
+	const contentMap = contentArrayResolved.reduce(
+		(content, piece) => ({ ...content, ...piece }),
+		{}
+	);
 
-  if (isDebug) {
-    console.log('\nContent\n', JSON.stringify(contentMap.main, null, 2));
-  }
+	if (isDebug) {
+		console.log("\nContent\n", JSON.stringify(contentMap.main, null, 2));
+	}
 
-  return contentMap;
+	return contentMap;
 };
 
 const getContentCheck = () => {
-  try {
-    return getContent();
-  } catch (err) {
-    console.error(err);
-  }
+	try {
+		return getContent();
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 module.exports = {
-  getContent: getContentCheck,
+	getContent: getContentCheck,
 };
